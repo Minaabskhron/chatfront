3cdc0143f706c9551adb6babfa42e9d57aa66e7e
minaabskhron <minaabskhron@gmail.com>
2025-05-10 12:53:51 +0300

    const 3

--- app/_utils/const.js
+++ app/_utils/const.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
 
-// export const baseUrl = "https://chat-production-0ee4.up.railway.app";
+export const baseUrl = "https://chat-production-96ee.up.railway.app";
